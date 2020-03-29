f6e0e4b68a1b227ad3d716c4833a3d8de70939d5
abdelilah <naimi201@hotmail.com>
2020-03-29 19:07:40 -0400

    Initial commit. create spring boot project and configure the application properties

---
+++ boukstore/src/main/java/nai/ca/bstr/BoukstoreApplication.java
@@ -0,0 +1,13 @@
+package nai.ca.bstr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoukstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoukstoreApplication.class, args);
+	}
+
+}
---
+++ boukstore/src/test/java/nai/ca/bstr/BoukstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package nai.ca.bstr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoukstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
